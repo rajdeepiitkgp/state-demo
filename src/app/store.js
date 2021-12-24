fa881b010f0c5c5e31a86a134b234d118bd77d2f
Rajdeep Biswas <rajdeep.biswas2010@gmail.com>
2021-12-24 22:41:52 +0530

    disable devtools in prod

--- src/app/store.js
+++ src/app/store.js
@@ -3,6 +3,7 @@ import { counter, account, customer } from '../features';
 
 const store = configureStore({
   reducer: { counter, account, customer },
+  devTools: process.env.NODE_ENV !== 'production',
 });
 
 export default store;
